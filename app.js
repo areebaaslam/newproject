ea295ac3dc75240c8233608f769a1bd35df57907
Areeba Aslam <110449436+areebaaslam@users.noreply.github.com>
2023-01-21 08:23:59 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+function readPara(){
+    var para = document.getElementById("para 2");
+    para.innerHTML = `with the Islamic Syllabus children will be able to learn about the basic foundations of Islam formally
+    such as Fiqh, Quran, Hadith, Tauhid, Arabic language, History of Islam and our Prophet (S) and the holy
+    Aimmah. The formal Islamic education will keep the values and basics of Islam ingrained into the life of a
+    child`
+    
+}
+readPara();
